693de10bb1c21088f339f570870c701939642f48
Pele Constam <pele.constam@gmail.com>
2026-04-18 09:37:52 +0800

    ctti

--- include/cli/ctti.hpp
+++ include/cli/ctti.hpp
@@ -11,10 +11,10 @@
 
 // clang-format off
 #if defined(__clang__) && !defined(_MSC_VER)
-  #define CTTI_TYPE_PRETTY_FUNCTION_PREFIX "CharView cli::ctti::dtl::name_impl() [T = "
+  #define CTTI_TYPE_PRETTY_FUNCTION_PREFIX "consteval CharView cli::ctti::dtl::name_impl() [T = "
   #define CTTI_TYPE_PRETTY_FUNCTION_SUFFIX "]"
 #elif defined(__GNUC__) && !defined(__clang__)
-  #define CTTI_TYPE_PRETTY_FUNCTION_PREFIX "cli::CharView cli::ctti::dtl::name_impl() [T = "
+  #define CTTI_TYPE_PRETTY_FUNCTION_PREFIX "consteval cli::CharView cli::ctti::dtl::name_impl() [T = "
   #define CTTI_TYPE_PRETTY_FUNCTION_SUFFIX "]"
 #elif defined(_MSC_VER)
   #if defined(__clang__)
@@ -53,13 +53,13 @@ namespace dtl {
 template <typename T> consteval CharView name_impl() {
 #if defined(__clang__) and not defined(_MSC_VER)
   constexpr CharView name{std::source_location::current().function_name()};
-  // static_assert(name.starts_with(CTTI_TYPE_PRETTY_FUNCTION_PREFIX));
+  static_assert(name.starts_with(CTTI_TYPE_PRETTY_FUNCTION_PREFIX));
   const auto split = name.substr(sizeof(CTTI_TYPE_PRETTY_FUNCTION_PREFIX),
                                  name.find_last_of("]"));
   return split;
 #elif defined(__GNUC__) and !defined(__clang__)
   constexpr CharView name{__PRETTY_FUNCTION__};
-  // static_assert(name.starts_with(CTTI_TYPE_PRETTY_FUNCTION_PREFIX));
+  static_assert(name.starts_with(CTTI_TYPE_PRETTY_FUNCTION_PREFIX));
   const auto split = name.substr(sizeof(CTTI_TYPE_PRETTY_FUNCTION_PREFIX));
   return split.substr(0, split.find_first_of(";"));
 #elif defined(_MSC_VER)
