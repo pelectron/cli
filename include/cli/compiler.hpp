f5159b636af0801dd376df96c860ee23c27c977f
Pele Constam <pele.constam@gmail.com>
2026-05-19 17:43:38 +0200

    reordered compiler check to make clangd work on windows

--- include/cli/compiler.hpp
+++ include/cli/compiler.hpp
@@ -2,19 +2,23 @@
 #define CLI_COMPILER_HPP
 
 // clang-format off
-#if defined (_MSC_VER)
-  #if defined(__clang__)
-    #define CLI_CLANG_CL
-  #else
-    #define CLI_MSVC
-  #endif
-#elif defined(__clang__)
+#if defined(__clang__)
   #define CLI_CLANG
 #elif defined(__GNUC__)
   #if defined (__arm__)
     #define CLI_ARM_GCC
   #else
     #define CLI_GCC 
+  #endif
+#elif defined (_MSC_VER)
+  #if defined(__clang__)
+    #if __has_cpp_attribute(no_unique_address)
+      #define CLI_CLANG
+    #else
+      #define CLI_CLANG_CL
+    #endif
+  #else
+    #define CLI_MSVC
   #endif
 #else 
 #error "Unsupported Compiler detected!"
